b3b80cd20804c27e7dbc8081d29d86f450fa07ca
Alejandro <alejandroarguelloduarte@gmail.com>
2021-03-06 17:58:17 -0500

    interactividad agregada

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,27 @@
+/*  menu */
 const navButton = document.getElementById('nav-toggle');
-const menu = document.querySelector('.nav__menu')
-const link = document.querySelectorAll('.nav__link')
+const menu = document.querySelector('.nav__menu');
+const link = document.querySelectorAll('.nav__link');
+
+/* modals */
+const modalButton = document.querySelectorAll('.button');
+const modalContainer = document.querySelector('.modal-container')
+const modalSelection = document.querySelector('.modal-selection')
+const modalCloseButton = document.querySelector('.modal__close')
+
+/*  modal stand */
+const modalStand = document.querySelectorAll('.modal-stand')
+const pledge = document.querySelectorAll('.pledge')
+
 /* event listeners */
 navButton.addEventListener('click', toggle);
 link.forEach(n => n.addEventListener('click', linkAction)) 
+
+modalButton.forEach(n => n.addEventListener('click', modalActive)) 
+modalCloseButton.addEventListener('click', close)
+
+modalStand.forEach(n => n.addEventListener('click', standSelected)) 
+
 
 
 function toggle() {
@@ -14,3 +32,17 @@ function linkAction() {
   menu.classList.remove('active')
 }
 
+function modalActive() {
+  modalContainer.classList.add('active')
+  modalSelection.classList.add('active')
+}
+
+function close() {
+  modalContainer.classList.remove('active')
+  modalSelection.classList.remove('active')
+}
+
+function standSelected() {
+  modalStand.forEach(n => n.classList.remove('active')) 
+  this.classList.toggle('active');
+}
